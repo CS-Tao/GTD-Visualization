a37f2bdf2009bd1993cc21e635ad47eb5365a00f
924797163@qq.com <924797163@qq.com>
2018-05-03 21:20:09 +0800

    修改eslint报错

--- src/renderer/utils/index.js
+++ src/renderer/utils/index.js
@@ -23,7 +23,7 @@ export function parseTime (time, cFormat) {
     s: date.getSeconds(),
     a: date.getDay()
   }
-  const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+  const timeStr = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
     let value = formatObj[key]
     if (key === 'a') return ['一', '二', '三', '四', '五', '六', '日'][value - 1]
     if (result.length > 0 && value < 10) {
@@ -31,7 +31,7 @@ export function parseTime (time, cFormat) {
     }
     return value || 0
   })
-  return time_str
+  return timeStr 
 }
 
 export function formatTime (time, option) {
@@ -81,7 +81,7 @@ export function getQueryObject (url) {
 export function getByteLen (val) {
   let len = 0
   for (let i = 0; i < val.length; i++) {
-    if (val[i].match(/[^\x00-\xff]/ig) != null) {
+    if (val[i].match(/[^\x00-\xff]/ig) != null) { // eslint-disable-line
       len += 1
     } else { len += 0.5 }
   }
