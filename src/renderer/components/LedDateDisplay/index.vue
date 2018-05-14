5efcddd6bf21e2547dd10a50e06714c07564cdbb
CS-Tao <990296951@qq.com>
2018-05-14 13:14:41 +0800

    更改日期控件

--- src/renderer/components/LedDateDisplay/index.vue
+++ src/renderer/components/LedDateDisplay/index.vue
@@ -2,47 +2,30 @@
   <div class="wrap">
     <div class="clock">
       <ul>
-        <li>
-          <ul id="led-date-time0">
+        <li v-for="m in 4" :key="m">
+          <ul :id="'led-date-time' + (m-1)">
             <li v-for="n in 7" :key="n">
-              <img :src="dateImgList[0][n-1]" width="100%" height="100%">
+              <img :src="dateImgList[m-1][n-1]" width="100%" height="100%">
             </li>
           </ul>
         </li>
-            <li>
-          <ul id="led-date-time1">
+
+        <li class="point"><span></span></li>
+
+        <li v-for="m in 2" :key="m+4">
+          <ul :id="'led-date-time' + ((m-1) + 4)">
             <li v-for="n in 7" :key="n">
-              <img :src="dateImgList[1][n-1]" width="100%" height="100%">
+              <img :src="dateImgList[m-1+4][n-1]" width="100%" height="100%">
             </li>
           </ul>
         </li>
-        <li class="point"><span></span><span></span></li>
-        <li>
-          <ul id="led-date-time2">
+
+        <li class="point"><span></span></li>
+
+        <li v-for="m in 2" :key="m+6">
+          <ul :id="'led-date-time' + ((m-1) + 6)">
             <li v-for="n in 7" :key="n">
-              <img :src="dateImgList[2][n-1]" width="100%" height="100%">
-            </li>
-          </ul>
-        </li>
-        <li>
-          <ul id="led-date-time3">
-            <li v-for="n in 7" :key="n">
-              <img :src="dateImgList[3][n-1]" width="100%" height="100%">
-            </li>
-          </ul>
-        </li>
-        <li class="point"><span></span><span></span></li>
-          <li>
-          <ul id="led-date-time4">
-            <li v-for="n in 7" :key="n">
-              <img :src="dateImgList[4][n-1]" width="100%" height="100%">
-            </li>
-          </ul>
-        </li>
-            <li>
-          <ul id="led-date-time5">
-            <li v-for="n in 7" :key="n">
-              <img :src="dateImgList[5][n-1]" width="100%" height="100%">
+              <img :src="dateImgList[m-1+6][n-1]" width="100%" height="100%">
             </li>
           </ul>
         </li>
@@ -69,13 +52,12 @@ export default {
   },
   data () {
     return {
-      BackImg,
-      FrontImg,
-      a: ['0 1 2 4 5 6', '2 5', '0 2 3 4 6', '0 2 3 5 6', '1 2 3 5', '0 1 3 5 6', '0 1 3 4 5 6', '0 2 5', '0 1 2 3 4 5 6', '0 1 2 3 5 6'],
-      b: ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五'],
-      dateStr: Date(),
-      weekStr: '',
+      backImg: BackImg,
+      frontImg: FrontImg,
+      numMap: ['0 1 2 4 5 6', '2 5', '0 2 3 4 6', '0 2 3 5 6', '1 2 3 5', '0 1 3 5 6', '0 1 3 4 5 6', '0 2 5', '0 1 2 3 4 5 6', '0 1 2 3 5 6'],
       dateImgList: [
+        [FrontImg, FrontImg, FrontImg, FrontImg, FrontImg, FrontImg, FrontImg],
+        [FrontImg, FrontImg, FrontImg, FrontImg, FrontImg, FrontImg, FrontImg],
         [FrontImg, FrontImg, FrontImg, FrontImg, FrontImg, FrontImg, FrontImg],
         [FrontImg, FrontImg, FrontImg, FrontImg, FrontImg, FrontImg, FrontImg],
         [FrontImg, FrontImg, FrontImg, FrontImg, FrontImg, FrontImg, FrontImg],
@@ -88,44 +70,41 @@ export default {
   computed: {
   },
   mounted () {
-    this.showTime()
-    setInterval(() => {
-      this.showTime()
-    }, 1000)
+  },
+  watch: {
+    date () {
+      this.showDate(this.date)
+    }
   },
   methods: {
-    showTime () {
-      var time = new Date()
-      var y = time.getFullYear()
-      var M = time.getMonth()
-      var d = time.getDate()
-      var h = time.getHours()
-      var m = time.getMinutes()
-      var s = time.getSeconds()
-      M = M + 1
-      var str = y.toString() + '年'
-      if (M < 10) { str = str + '0' + M.toString() + '月' } else { str = str + M.toString() + '月' }
-      if (d < 10) { str = str + '0' + d.toString() + '日' } else { str = str + d.toString() + '日' }
+    showDate (date) {
+      let y = date.getFullYear()
+      let m = date.getMonth()
+      let d = date.getDate()
+      m = m + 1
 
-      this.dateStr = str
-      this.weekStr = this.b[time.getDay()]
-      this.modiflyNum(0, parseInt(h / 10))
-      this.modiflyNum(1, h % 10)
-      this.modiflyNum(2, parseInt(m / 10))
-      this.modiflyNum(3, m % 10)
-      this.modiflyNum(4, parseInt(s / 10))
-      this.modiflyNum(5, s % 10)
+      let y1 = parseInt(y / 1000) // 千
+      let y2 = parseInt((y - y1 * 1000) / 100) // 百
+      let y3 = parseInt((y - y1 * 1000 - y2 * 100) / 10) // 十
+      let y4 = parseInt(y - y1 * 1000 - y2 * 100 - y3 * 10) // 个
+
+      this.modiflyNum(0, y1)
+      this.modiflyNum(1, y2)
+      this.modiflyNum(2, y3)
+      this.modiflyNum(3, y4)
+      this.modiflyNum(4, parseInt(m / 10))
+      this.modiflyNum(5, m % 10)
+      this.modiflyNum(6, parseInt(d / 10))
+      this.modiflyNum(7, d % 10)
     },
     modiflyNum (id, value) {
-      var str = this.a[value]
+      var str = this.numMap[value]
       var cc = str.split(' ')
       for (let i = 0; i < 7; i++) {
-        this.$set(this.dateImgList[id], i, this.BackImg)
-        // this.dateImgList[id][i] = this.BackImg
+        this.$set(this.dateImgList[id], i, this.backImg)
       }
       for (let i = 0; i < cc.length; i++) {
-        this.$set(this.dateImgList[id], parseInt(cc[i]), this.FrontImg)
-        // this.dateImgList[id][parseInt(cc[i])] = this.FrontImg
+        this.$set(this.dateImgList[id], parseInt(cc[i]), this.frontImg)
       }
     }
   }
@@ -151,7 +130,7 @@ li{list-style-type: none;}
 .wrap .clock ul li ul li:nth-child(6){position:absolute;transform:rotateZ(90deg);left:65px;top: 150px;}
 .wrap .clock ul li ul li:nth-child(7){position:absolute;top: 200px;left: 15px;}
 .wrap .clock ul .point{width: 30px;}
-.wrap .clock ul .point span{display:block;height: 20px;width: 20px;background: #a3ff19;margin:60px 15px;}
+.wrap .clock ul .point span{display:block; position: absolute; height: 20px;width: 20px;background: #a3ff19;margin:0px 15px; bottom: 0px }
 .wrap .date{position:relative;height: 100px; color: #a3ff19; font-size:80px;margin-top: 10px;border-bottom:3px solid red;}
 .wrap .date span:first-child{float: left;margin-left: 10px;}
 .wrap .date span:last-child{float: right;margin-right:30px;}
